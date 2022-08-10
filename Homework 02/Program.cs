c5a174ca06bbe88c226502f62a0892ad31b8b948
katy0306 <Sarafanchuk@mail.ru>
2022-08-11 01:56:15 +0300

    Первое задание

---
+++ Homework 02/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Напишите программу, которая на вход принимает два числа и выдаёт максимальное
+        Console.WriteLine("Введите первое число:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число:");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        if (a > b)
+        {
+            Console.WriteLine($" max {a}");
+        }
+        else
+        {
+            Console.WriteLine($" max {b}");
+        }
+    }
+}
