11dfbb14fac898997f7df5f148c142e182b3ab1f
katy0306 <Sarafanchuk@mail.ru>
2022-08-11 21:46:20 +0300

    Семинар второй

--- Seminar2_000/Program.cs
+++ Seminar2_000/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See https://Напишите программу которая принимает на вход два числа, и проверяет, являеться ли одно число квадратом другого
+Console.WriteLine( "Введите число a" );
+int a = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine( "Введите число b" );
+int b = Convert.ToInt32(Console.ReadLine());
+
+if ((a * a == b)^(b * b ==a ))
+{
+Console.WriteLine( "Да" );
+}
+else
+Console.WriteLine( "Нет" );
