61e7a017f6eb17288f0f1b8e99e1fc1146aa2129
katy0306 <Sarafanchuk@mail.ru>
2022-08-15 21:44:21 +0300

    Homework_Task10

--- Homework_Task10/Program.cs
+++ Homework_Task10/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число ");
+int number =Convert.ToInt32(Console.ReadLine());
+
+if ((number > 99) & (number < 1000))
+{
+   int a = number / 10;
+   int b = a % 10;
+   Console.WriteLine(b);
+
+}
+else
+Console.WriteLine("Число не трехзначное");  
