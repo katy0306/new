a04d397b267c93234cdbe52ee6eb90939bc32b75
katy0306 <Sarafanchuk@mail.ru>
2022-08-23 16:53:19 +0300

    Homework_Task29

---
+++ Homework_Task29/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+try
+{
+    int [] array = new int [8];
+    for(int i = 0; i < 8; i++)
+    {
+        Console.WriteLine($"Введите число с индексом  {i}");
+        array[i] = int.Parse(Console.ReadLine());
+        
+
+
+    }
+    for(int j = 0; j < 8; j++)
+    {
+        Console.Write($"{array[j]} ");
+    }
+}
+catch
+{
+   Console.WriteLine("Не верно введены числа");
+ 
+}
